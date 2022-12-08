9e4348811a06712e5e62b654a7f8b402d12bcb5c
Fonnt <dballesterfont@gmail.com>
2022-12-08 15:29:32 +0100

    Deleted an extra / in the api path

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,3 +1,3 @@
-const BASE_URL = "https://project-auth-kyvbhopdlq-lz.a.run.app/";
+const BASE_URL = "https://project-auth-kyvbhopdlq-lz.a.run.app";
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`;
