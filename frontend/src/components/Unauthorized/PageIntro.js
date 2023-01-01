3a1f9dde1eee2bf30b116e64f63cd41952aef3f3
miadahlgren <101955676+miadahlgren@users.noreply.github.com>
2023-01-01 16:51:01 +0100

    added timeline images, new components

--- frontend/src/components/Unauthorized/PageIntro.js
+++ frontend/src/components/Unauthorized/PageIntro.js
@@ -1,20 +1,25 @@
 import React from "react";
 import styled from "styled-components";
 import infinity from "assets/icons/icon.svg";
-import TimeLineRow from "./TimelineRow";
+import { TimeLineRowLeft , TimeLineRowRight, ContentText1, ContentText2, ContentText3 } from "./TimelineRow";
+import flowers from "assets/flowers.jpg";
+import cheers from "assets/cheers.jpg";
+import flowersandring from "assets/flowersandring.jpg";
 
 
+const contentText1 = "What is Blissful Beginnings?";
 
-
-const PageIntro = () => {
+const PageIntro = (image, contentText) => {
 return( <><StyledIntro><div className="intro-div">
     <StyledSubHeading><h3>some subheading</h3></StyledSubHeading>
     <StyledHeading><h2>Welcome</h2></StyledHeading>
     <div className="img-div"><img src={infinity} className="infinity"/></div>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam suscipit felis sapien, ac suscipit velit ornare id. Aliquam vulputate vel metus quis condimentum. Vivamus bibendum orci eget nisi pulvinar iaculis. Maecenas non eros ut erat dictum dapibus. Morbi quis elit libero. Fusce id quam vel orci rhoncus auctor. Suspendisse ac metus tortor. Sed scelerisque velit sed ultricies maximus. Morbi nisi elit, scelerisque sed leo vitae, rhoncus pellentesque elit. Quisque turpis nibh, pulvinar ac nisi vitae, fermentum finibus felis. Quisque id eros eu augue imperdiet imperdiet sed sit amet libero. In in luctus purus. Curabitur tincidunt, elit a porta blandit, orci.</p>
 </div>
-<div><TimeLineRow />
-<TimeLineRow /><TimeLineRow /></div>
+<div><TimeLineRowLeft image = {flowers} contentText = {<ContentText1 />} />
+<TimeLineRowRight image = {cheers} contentText = {<ContentText2 />} />
+<TimeLineRowLeft image= {flowersandring} contentText = {<ContentText3 />}/>
+</div>
 </StyledIntro>
 
 </>
