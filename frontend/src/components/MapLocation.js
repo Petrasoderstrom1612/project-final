66fcc8debf9c852fd3f285061f7e6b7e4045951e
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2023-01-02 08:33:15 +0100

    added comments to the map component

--- frontend/src/components/MapLocation.js
+++ frontend/src/components/MapLocation.js
@@ -1,32 +1,31 @@
-/* import React from "react";
+import React from "react";
 
 const MapMarker = ({ text }) => <div>{text}</div>;
 
 export default function MapLocation(){
   const defaultProps = {
-    center: {
+    center: { //here will be the PROPS from the wedding couple
       lat: 59.327451,
       lng: 18.054346
     },
-    zoom: 17
+    zoom: 17 //the zoom needs to be included so that the map works, the number can be adjusted 
   };
 
   return (
     // Important! Always set the container height explicitly
-      // Important! Always set the container height explicitly
     <>
       <h2>The venue</h2>
-      <div style={{ height: '60vh', width: '60%' }}>
+      <div style={{ height: '60vh', width: '60%' }}> //CSS to be moved
         <GoogleMapReact
-          bootstrapURLKeys={{ key: "AIzaSyB8HH4BIwAaN0DV9Dol5AxvyMGfSp4L0Vw" }}
+          bootstrapURLKeys={{ key: "AIzaSyB8HH4BIwAaN0DV9Dol5AxvyMGfSp4L0Vw" }} //my google maps key
           defaultCenter={defaultProps.center}
           defaultZoom={defaultProps.zoom}
         >
-          <MapMarker
+          <MapMarker  //here will be the PROPS from the wedding couple
             lat={59.327451}
             lng={18.054346}
             text="🤍" />
         </GoogleMapReact>
       </div></>
   );
-} */
+}
