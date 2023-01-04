57d95fa1c846c03a8b2c7ae2ab163caba0e748a0
miadahlgren <101955676+miadahlgren@users.noreply.github.com>
2023-01-04 10:15:36 +0100

    updated the wedding form with new input fields

--- frontend/src/components/Authorized/WeddingForm.js
+++ frontend/src/components/Authorized/WeddingForm.js
@@ -14,37 +14,31 @@ const WeddingForm = () => {
 					<h3>Wedding form</h3>
 					<h2>Fill in the following form below in order to create your dreamy website.</h2>
 					<div className="img-div"><img src={infinity} className="infinity" /></div>
-					<form className="RSVP-form">
-						<label>
-							<input className="RSVP-input" type="text" placeholder="Your First Name" />
+					<form className="wedding-form">
+						<label>Information about the Wedding Couple<br/><br/>
+							<input className="wedding-input" type="text" placeholder="Person 1 First Name" />
 						</label><br /><br />
 						<label>
-							<input className="RSVP-input" type="text" placeholder="Your Last Name" />
+							<input className="wedding-input" type="text" placeholder="Person 1 Last Name" />
 						</label><br /><br />
 						<label>
-							<input className="RSVP-input" type="text" placeholder="Your e-mail" /></label><br /><br />
-						<label> Attending?</label><br></br>
-						<input type="radio" id="attending" name="attending" value="Yes" />
-						<label for="attending">Yes, I'll be there</label>
-						<input type="radio" id="not-attending" name="attending" value="No" />
-						<label for="not-attending">Sorry, I can't come</label><br /><br />
-
-						<label> Accommodation</label><br />
-						<input type="radio" id="accommodation-help" name="accommodation" value="accommodation-help" />
-						<label for="accommodation-help">I need help finding accommodation</label>
-						<input type="radio" id="accommodation-done" name="accommodation" value="accommodation-done" />
-						<label for="accommodation-done">I already have accommodation</label><br /><br />
-
-						<select className="RSVP-input" name="food-restrictions" id="food-restrictions">
-							<option value="" disabled selected>Food Restrictions</option>
-							<option value="Gluten Free">Gluten Free</option>
-							<option value="Lactose Free">Lactose Free</option>
-							<option value="Vegetarian">Vegetarian</option>
-							<option value="Vegan">Vegan</option>
-							<option value="Other">Other. Please specify below</option>
-						</select>
+							<input className="wedding-input" type="text" placeholder="Person 2 First Name" />
+						</label><br /><br />
 						<label>
-							<input className="RSVP-input message" type="text" placeholder="Any additional information you would like to add?" /></label><br /><br />
+							<input className="wedding-input" type="text" placeholder="Person 2 Last Name" />
+						</label><br /><br />
+						<label>
+							<input className="wedding-input" type="text" placeholder="Your e-mail" /></label><br /><br />
+							<label>Wedding Info<br/><br/>							
+							<input className="wedding-input" type="date" placeholder="Date of the Wedding"/></label><br /><br />
+							<label>
+							<input className="wedding-input" type="time" placeholder="Time of the Wedding"/></label><br /><br />
+							<label>
+							<input className="wedding-input" type="text" placeholder="Location of the Wedding Ceremony" /></label><br /><br />
+						<label>
+							<input className="wedding-input" type="text" placeholder="Location of the Wedding Party" /></label><br /><br />
+						<label>
+							<input className="wedding-input message" type="text" placeholder="Add some information about the location" /></label><br /><br />
 						<StyledButton type="submit">Send</StyledButton>
 					</form>
 				</StyledDiv>
