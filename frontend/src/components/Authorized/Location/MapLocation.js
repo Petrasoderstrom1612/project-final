cf40e9841b2716a45a2613ca5ece2242a966a88a
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2023-01-04 16:31:30 +0100

    made the Location component responsive on all devices

--- frontend/src/components/Authorized/Location/MapLocation.js
+++ frontend/src/components/Authorized/Location/MapLocation.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from 'react';
+import styled from "styled-components/macro";
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
 const containerStyle = { //CSS can be moved
-  width: '400px',
-  height: '400px'
+  width: '80vw',
+  height: '50vh',
 };
+
 
 const center = {
   lat: 59.327451,
   lng: 18.054346
 };
 
-function MapLocation() {
+const MapLocation= () => {
   return (
+    <MapStyler>
     <LoadScript
       googleMapsApiKey="AIzaSyB8HH4BIwAaN0DV9Dol5AxvyMGfSp4L0Vw"
     >
@@ -22,10 +25,17 @@ function MapLocation() {
         zoom={17}
       >
       <Marker position={{ lat: 59.327451, lng: 18.054346}} />
-        <></>
+
       </GoogleMap>
     </LoadScript>
+    </MapStyler>
   )
 }
 
 export default React.memo(MapLocation)
+
+export const MapStyler = styled.div`
+filter: grayscale(100%) brightness(90%) contrast(1);
+width: '400px';
+height: '400px';
+`
