9384bd89fb774cc5b423ac2e7c6ea68dba8fe542
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2023-01-04 13:41:22 +0100

    Added map in the location

--- backend/controllers/guestaccess.js
+++ backend/controllers/guestaccess.js
@@ -1,31 +1,29 @@
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 
-const { Wedding } = require('../models/models.js')
+const { Wedding } = require("../models/models.js");
 
-
-//Vi måste validera mot rätt wedding-id på något sätt här.... 
+//Vi måste validera mot rätt wedding-id på något sätt här....
 
 const guestpassword = Wedding.guestpassword;
 //Function for authenticate guest const authenticateUser = async (req, res, next) => {
-  export const giveGuestAccess = async (req, res) => {
-    const guestAccess = req.body;
-    try {
-if (guestAccess === guestpassword){
-        res.status(200).json({
-          success: true,
-          response: 'you are allowed in'
-          
-        });
-      } else {
-        res.status(400).json({
-          success: false,
-          response: "Not correct password"
-        });
-    }
+export const giveGuestAccess = async (req, res) => {
+const guestAccess = req.body;
+try {
+if (guestAccess === guestpassword) {
+res.status(200).json({
+success: true,
+response: "you are allowed in",
+});
+} else {
+res.status(400).json({
+success: false,
+response: "Not correct password",
+});
+}
 } catch (error) {
-      res.status(500).json({
-        success: false,
-        response: error
-      });
-    }
-  };
+res.status(500).json({
+success: false,
+response: error,
+});
+}
+};
--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -1,32 +1,31 @@
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 
-const { User } = require('../models/models.js');
+const { User } = require("../models/models.js");
 
+export const loginUser = async (req, res) => {
+  const { username, password } = req.body;
 
- export const loginUser = async (req, res) => {
-    const { username, password } = req.body;
-  
-    try {
-      const user = await User.findOne({ username });
-      if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({
-          success: true,
-          response: {
-            username: user.username,
-            id: user._id,
-            accessToken: user.accessToken
-          }
-        });
-      } else {
-        res.status(400).json({
-          success: false,
-          response: "Credentials didn't match"
-        });
-      }
-    } catch (error) {
-      res.status(500).json({
+  try {
+    const user = await User.findOne({ username });
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({
+        success: true,
+        response: {
+          username: user.username,
+          id: user._id,
+          accessToken: user.accessToken,
+        },
+      });
+    } else {
+      res.status(400).json({
         success: false,
-        response: error
+        response: "Credentials didn't match",
       });
     }
-  };
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      response: error,
+    });
+  }
+};
