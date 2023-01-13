32b7a4431a1fce5b3ab7071128c22b7393817fa1
Julia Ostedt <julia.ostedt@gmail.com>
2023-01-13 09:08:40 +0100

    adjusted post in weddingform

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,5 +1,5 @@
-const BASE_URL = "https://project-final-4ei4tu6anq-lz.a.run.app";
-// const BASE_URL = "http://localhost:8090";
+// const BASE_URL = "https://project-final-4ei4tu6anq-lz.a.run.app";
+const BASE_URL = "http://localhost:8090";
 
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`;
